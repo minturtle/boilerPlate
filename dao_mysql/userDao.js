78b0d933b1ce568b3daa135804f29b6486bbf27e
minturtle <minseok0425@kakao.com>
2022-09-03 13:41:24 +0900

    user save 기능 구현

--- dao_mysql/userDao.js
+++ dao_mysql/userDao.js
@@ -1,15 +1,28 @@
 const mysql = require("mysql");
 const mysqlConnectionData = require('./mysqlConnectionData');
 
-
+const util = require('util');
 const connection = mysql.createConnection(mysqlConnectionData)
 
 
 const e = {
     findAll : (callback)=>{
         connection.query("SELECT * FROM user", callback);
+    },
+
+
+    //registerJson : {userId, password, userName}
+    save : (registerJson , callback)=>{
+        const insertQuery = createInsertQuery(registerJson);
+
+        connection.query(insertQuery, callback);
     }
 }
 
+function createInsertQuery(registerjson){
+
+    return util.format("INSERT INTO user(user_id, user_name, password) VALUES ('%s', '%s', '%s');"
+    , registerjson.userId, registerjson.userName, registerjson.password);
+}
 
 module.exports = e;
--- index.js
+++ index.js
@@ -2,12 +2,18 @@ const express = require('express');
 
 const app = express();
 const userDao = require('./dao_mysql/userDao');
-
+const bodyParser = require('body-parser');
 const SERVER_PORT = 5000;
 
 
+//application/x-www-form-urlencode
+app.use(bodyParser.urlencoded({extended:true}));
+
+//application/json
+app.use(bodyParser.json());
 
 app.listen(SERVER_PORT , ()=>console.log("server is running at "+ SERVER_PORT));
+
 
 
 app.get("/", (req, res)=>{
@@ -17,8 +23,29 @@ app.get("/", (req, res)=>{
 
 app.get("/users", (req, res)=>{
     userDao.findAll((err, rows, fields)=>{
-        if(err) throw err;
-        res.status(200).json(rows)
+        if(err) { 
+            sendErrorMsg(err, res);
+            return;
+        }
+        res.status(200).json(rows);
     })
 
 })
+
+app.post("/register", (req, res)=>{
+    const registerJson = req.body;
+    userDao.save(registerJson, (err, rows, fields)=>{
+        if(err) { 
+            sendErrorMsg(err, res);
+            return;
+        }
+        res.redirect("/");
+    })
+
+})
+
+
+function sendErrorMsg(err, res){
+        console.log(err);
+        res.status(500).json({err : "서버 오류입니다."})
+}
